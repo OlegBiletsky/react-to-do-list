be676492b03657249cd10170974302581570c0ca
OlegBiletsky <ol.biletsky@gmail.com>
2020-11-03 16:34:09 +0200

    solution3

--- src/components/list/addButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function AddButton(props) {
-    return(
-            <button onClick={props.handleAddItemToList}>Додати</button>
-    )
-}
-export default AddButton;
--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import ResetButton from './resetButton'
-import AddButton from './addButton'
-import InputField from './inputField'
 
 class List extends React.Component {
     constructor() {
@@ -10,96 +7,133 @@ class List extends React.Component {
             currentItem: {
                             text: "",
                             // id: "",
-                            key:"",
                             completed: false,
                         },
-            list: [],
+            list: [
+                {
+                    text: "Vita",
+                    id: 1,
+                    completed: false,
+                },
+                {
+                    text: "Oleh",
+                    id: 2,
+                    completed: false,
+                },
+                {
+                    text: "sasa",
+                    id: 3,
+                    completed: false,
+                },
+                {
+                    text: "zirka",
+                    id: 4,
+                    completed: false,
+                },
+                {
+                    text: "papa",
+                    id: 5,
+                    completed: false,
+                },
+                {
+                    text: "mama",
+                    id: 6,
+                    completed: false,
+                },
+            ],
         }
         this.handleInputChange = this.handleInputChange.bind(this) 
-        this.handleAddItemToList = this.handleAddItemToList.bind(this) 
+        this.handleAddItemToList = this.handleAddItemToList.bind(this)
+        this.handleRenderList = this.handleRenderList.bind(this) 
         this.handleResetList = this.handleResetList.bind(this) 
         this.handleDeleteItem = this.handleDeleteItem.bind(this) 
-        // this.handleItemDone = this.handleItemDone.bind(this)
+        this.handleItemDone = this.handleItemDone.bind(this)
     }
 
     handleInputChange(e) {
             this.setState({
                     currentItem: {
                         text: e.target.value,
-                        key: Date.now(),
+                        // id: "",
                         completed: false,
                     }                    
             })
     }
-    handleAddItemToList() {
+    handleAddItemToList(e) {
+        e.preventDefault();
         this.setState(
             state => {
-                const newList = [...this.state.list, this.state.currentItem.text]
+                const newList = [...this.state.list, this.state.currentItem]
                 return{
-                    currentItem: {
+                    currentItem:{
                         text: "",
-                        key:"",
+                        // id: "",
                         completed: false,
                     },
-                    list: newList,
+                    list: newList
                 }
             }
         )
-        
     }
     handleRenderList() {
-        const visibleList = this.state.list.map( 
-                (i,index) => {
-                                <li key={index}>
-                                {i} 
+        const visibleList = this.state.list.map(
+                    (i, index) => (
+                            <li>
+                                {i.text}
+
                                 <button id={index} onClick={this.handleItemDone}>Зроблено</button> 
                                 <button id={index} onClick={this.handleDeleteItem}>Видалити</button> 
-                                </li>
-                            }
-                    )
+                            </li>
+                        )
+        )
         return(visibleList)
     }
-
     handleDeleteItem(e) {
         this.setState(
             state => {
-            const newList = this.state.list.filter( (i,index) => index != e.target.id)
+            const newList = this.state.list.filter( (i,index) => index != e.target.id )
                 return{
-                    currentItem: {
-                        text: e.target.value,
+                    currentItem:{
+                        text: "",
                         // id: "",
-                        key:"",
                         completed: false,
                     },
-                    list: newList,
+                    list: newList
                 }
+            }
+        )
+    }
+    handleItemDone(e) {
+        this.setState(
+            state => {
+                this.state.list.forEach( i => {
+                    i.id == e.target.id ? i.completed=!i.completed : i.completed;
+                    }     
+                )
             }
         )
     }
     handleResetList() {
-            this.setState({
-                currentItem: {
-                    text: "",
-                    // id: "",
-                    key:"",
-                    completed: false,
-                },
-                list: [],
-            })
+        this.setState({
+            list: []
+        })
     }
-    
-
-
     render() {
 
         return(
             <>
-                <ResetButton handleResetList={this.handleResetList} />   
-                <InputField 
-                    value={this.state.currentItem.text}
-                    onChange={this.handleInputChange}
-                />        
-                <AddButton handleAddItemToList={this.handleAddItemToList} />
+                <button onClick={this.handleResetList}>Очистити все!</button>
+
+                <form onSubmit={this.handleAddItemToList}>
+                    <input 
+                        type='text'
+                        placeholder="add new task"
+                        value={this.state.currentItem.text}
+                        onChange={this.handleInputChange}
+                    />
+                </form>
+                 
+                <button onClick={this.handleAddItemToList}>Додати</button>
 
                 {this.handleRenderList()}
             </>
@@ -107,3 +141,4 @@ class List extends React.Component {
     }
 }
 export default List;
+
