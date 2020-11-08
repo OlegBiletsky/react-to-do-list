57b651bd8b8ff3aa0fc42b99d0df071a47a6b965
OlegBiletsky <ol.biletsky@gmail.com>
2020-11-09 00:10:24 +0200

    maybe done)

--- src/components/listItem/listItem.js
+++ src/components/listItem/listItem.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './listItem.css';
+import cn from 'classnames'
 
 // {this.state.list.map((item) => (//для кожного айтема з ліста малюємо наступне
 //     <ListItem
@@ -36,28 +38,38 @@ class ListItem extends React.Component {
             }
             
         };
-      
+        
+        let completedButton = cn({
+            completedButton: true,
+            completedButton_true: completed,
+            completedButton_false: !completed,
+        });
+        let editButton = cn({
+            editButton: true,
+            editButton_true: this.state.editing,
+        });
         return (
-            <li>
+            <li className='list-item'>
                 { 
                     this.state.editing ?  //ми редагуємо чи ні?? editing true or false? за замовчуванням стоїть false
-                        (<input       //якщо true то малюємо такий ІНПУТ
+                        (<input      //якщо true то малюємо такий ІНПУТ
                         value={this.state.text}  //цей text приходить з айтема в пропсах
                         onChange={(e) => this.setState({ text: e.target.value })} //реагуємо на тайпання
                         />) 
                     : 
                     completed ? //якщо editing==false, тобто не редагуємо то перевірка чи виконано? (completed: false з пропсів)
-                    (<strike>{text}</strike>) : //якщо completed: true, тобто виконано то перекреслюємо
-                    (<span onClick={() => onEdit()}>{text}</span>  ) //якщо completed: false, то просто text, той що з пропсів
+                    (<span className='completedItem' onClick={() => onEdit()}>{text}</span>) : //якщо completed: true, тобто виконано то перекреслюємо
+                    (<span className='item-usual' onClick={() => onEdit()}>{text}</span>  ) //якщо completed: false, то просто text, той що з пропсів
                 }
 
-                <button onClick={() => handleComplete(id)}>
+                <button className={completedButton} onClick={() => handleComplete(id)}>
                 {completed ? "Довиконувати" : "Зроблено"}
                 </button>
 
-                <button onClick={() => handleDelete(id)}>Видалити</button>
+                <button className='deleteButton' onClick={() => handleDelete(id)}>Видалити</button>
 
-                <button onClick={() => onEdit()}>
+                <button className={editButton} 
+                onClick={() => onEdit()}>
                 {this.state.editing ? "Зберегти" : "Редагувати"}
                 </button>
             </li>
